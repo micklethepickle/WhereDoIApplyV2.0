e35992c046e7bc9d99d105152cf5b484cb8caff3
Edward Son <edwardson@Edwards-MacBook-Air.local>
2016-02-29 23:56:36 -0500

    Creating caUnis

--- routes/index.js
+++ routes/index.js
@@ -17,12 +17,33 @@ var unis = mongoose.model('unis',
 			 physical : Number,
 			 dent: Number,
 			 mecheng: Number});
+var caUnis = mongoose.model('caUnis', 
+		{name : String,
+			url : String,
+		 	law : Number,
+		 	accounting :Number,
+		 	cs : Number,
+		 	commerce : Number,
+			 business : Number,
+			 ee : Number,
+			 management : Number,
+			 med : Number,
+			 health : Number,
+			 se : Number,
+			 physical : Number,
+			 dent: Number,
+			 mecheng: Number});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CHOOSE YOUR FIELD' });
 });
 /* GET findUnis page*/
 router.get('/findUnis', function(req, res, next) {
+  res.render('findUnis', { title: 'CHOOSE YOUR FIELD' });
+});
+
+/* GET findCaUnis page*/
+router.get('/findCaUnis', function(req, res, next) {
   res.render('findUnis', { title: 'CHOOSE YOUR FIELD' });
 });
 
@@ -61,5 +82,26 @@ router.post('/availableUnis', function(req, res) {
 		});
 })
 
+/* POST availableCaUnis page */
+router.post('/availableCaUnis', function(req, res) {
+	var aUnis = [];
+	var aUnisUrl = [];
+	var db = req.db;
+	var program = req.body.program;
+	var rScore = req.body.grade;
+		caUnis.find(function(err,uni) {
+			if (err) return console.error(err);
+			for (var i in uni){
+
+				if (rScore > uni[i][program]){
+					aUnis.push(uni[i].name);
+					aUnisUrl.push(uni[i].url);
+				};
+			};
+		console.log(rScore);
+		console.log(aUnisUrl);
+		res.render('availableCaUnis', {"availableCaUnis": aUnis, "aUnisUrl": aUnisUrl});
+		});
+})
 
 module.exports = router;

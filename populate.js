baeb2d10f8b404d5334e88a14a46b531d0de7dbc
Ubuntu <ubuntu@ip-172-31-57-24.ec2.internal>
2016-03-01 01:49:06 +0000

    added gitignore

---
+++ populate.js
@@ -0,0 +1,14 @@
+db.unis.insert({"name" : "McGill", "mecheng" : 29.4, "law" : 30.75, "accounting" : 28.8, "cs" : 28, "commerce" : 28.8, "business" : 28.8, "ee" : 28.5, "management" : 28.8, "med" : 32.24, "health" : 28, "se" : 28.5, "physical" : 28, "dent" : 32.21, "url" : "https://horizon.mcgill.ca/pban1/hzskalog.P_DISPLANGUECHOICE"});
+
+db.unis.insert({"name" : "Concordia", "mecheng" : 25, "law" : 100, "accounting" : 26, "cs" : 25, "commerce" : 26, "business" : 26, "ee" : 24, "management" : 26, "med" : 69, "health" : 20, "se" : 25, "physical" : 15, "dent" : 100, "url" :"https://www.concordia.ca/admissions.html"});
+
+db.unis.insert({"name" : "UQAM", "mecheng" : 100, "law" : 29.9, "accounting" : 15, "cs" : 15, "commerce" : 21, "business" : 15, "ee" : 100, "management" : 21, "med" : 69, "health" : 21, "se" : 100, "physical" : 15, "dent" : 100, "url" : "http://www.etudier.uqam.ca/admission"});
+
+db.unis.insert({"name" : "University of Montreal", "mecheng" : 100, "law" : 31, "accounting" : 28.8, "cs" : 100, "commerce" : 22, "business" : 28.8, "ee" : 100, "management" : 25, "med" : 33.39, "health" : 24, "se" : 100, "physical" : 22, "dent" : 32.35, "url" : "https://admission.umontreal.ca/admission/"});
+
+db.unis.insert({"name" : "Polytechnique", "mecheng" : 26, "law" : 100, "accounting" : 100, "cs" : 100, "commerce" : 100, "business" : 100, "ee" : 26, "management" : 100, "med" : 100, "health" : 100, "se" : 26, "physical" : 100, "dent" : 100, "url" : "http://www.polymtl.ca/admission/en"});
+
+db.unis.insert({"name" : "H.E.C.", "mecheng" : 100, "law" : 100, "accounting" : 26.5, "cs" : 100, "commerce" : 26.5, "business" : 26.5, "ee" : 100, "management" : 26.5, "med" : 100, "health" : 100, "se" : 100, "physical" : 100, "dent" : 100, "url" : "http://www.polymtl.ca/admission/en"});
+
+db.unis.insert({"name" : "Sherbrooke", "mecheng" : 26.4, "law" : 29.4, "accounting" : 100, "cs" : 15, "commerce" : 23.1, "business" : 23.1, "ee" : 22.1, "management" : 23.1, "med" : 33.3, "health" : 15, "se" : 20, "physical" : 15, "dent" : 100, "url" : "http://www.usherbrooke.ca/admission/"});
+
